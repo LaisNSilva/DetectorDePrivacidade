a83bae63e367a5173297d297057583bb5a9f0407
LaisNSilva <49496164+LaisNSilva@users.noreply.github.com>
2022-05-20 12:24:35 -0300

    popup

---
+++ beastify/popup/choose_beast.js
@@ -0,0 +1,18 @@
+document.addEventListener("click", function(e) {
+  if (!e.target.classList.contains("beast")) {
+    console.log(document.cookie);
+    return;
+  }
+
+  var chosenBeast = e.target.textContent;
+
+
+  chrome.tabs.executeScript(null, {
+    file: "/content_scripts/beastify.js"
+  });
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {beast: chosenBeast});
+  });
+
+});

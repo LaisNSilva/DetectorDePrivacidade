967c41955b9628162df9c8e8a30222c323cfa0e5
LaisNSilva <49496164+LaisNSilva@users.noreply.github.com>
2022-06-10 19:51:48 -0300

    ainda não

--- plugin/tab.js
+++ plugin/tab.js
@@ -1,49 +1,69 @@
 // Code based on https://github.com/mdn/webextensions-examples/tree/master/list-cookies
-function showTab(tabs) {
-    let tab = tabs.pop();
-    var tablist = document.getElementById('tab');
-    let li = document.createElement("li");
-    let content = document.createTextNode(tab.keys());
-    li.appendChild(content);
-    let li2 = document.createElement("li");
-    let content2 = document.createTextNode(tab.values());
-    li2.appendChild(content2);
-    tablist.appendChild(li2);
-    // var gettingAllCookies = browser.cookies.getAll({url: tab.url});
-    // gettingAllCookies.then((cookies) => {
-  
-    //   var activeTabUrl = document.getElementById('header-title');
-    //   var text = document.createTextNode("Cookies at: " + tab.title);
-    //   var cookieList = document.getElementById('cookie-list');
-    //   activeTabUrl.appendChild(text);
-  
-    //   if (cookies.length > 0) {
-    //     for (let cookie of cookies) {
-    //       let li = document.createElement("li");
-    //       let content = document.createTextNode(cookie.name + ": "+ cookie.value);
-    //       li.appendChild(content);
-    //       cookieList.appendChild(li);
-    //     }
-  
-    //     let quantity = document.createElement("p");
-    //     let quantityText = document.createTextNode("Total cookies: " + cookies.length);
-    //     quantity.appendChild(quantityText);
-    //     cookieList.appendChild(quantity);
-  
-    //   } else {
-    //     let p = document.createElement("p");
-    //     let content = document.createTextNode("No cookies in this tab.");
-    //     let parent = cookieList.parentNode;
-  
-    //     p.appendChild(content);
-    //     parent.appendChild(p);
-    //   }
-    //});
-  }
-  
-  function getActiveTab() {
-    return browser.tabs.query({currentWindow: true, active: true});
-  }
-  
-  getActiveTab().then(showTab);
-  
+
+function method3(url) {
+  var xhr = new XMLHttpRequest();
+  xhr.onload = function () {
+      console.log(xhr.responseText);
+  };
+  xhr.open("GET", url);
+  xhr.send();
+}
+
+function method2(id) {
+  chrome.tabs.sendMessage(id, {action: "getSource"}, function(response) {
+      console.log(response.sourceCode);
+  });
+}
+
+function showCookiesForTab(tabs) {
+  let tab = tabs.pop();
+
+  //var gettingAllLink =browser.get({url: tab.url});
+  //method1(tab.id);
+  //gettingAllLink.then(() => {
+    //var results = await browser.tabs.get(info.tab.id)
+     //method2(tab.id)
+    //var lis = tab.getElementsByTagName("li");
+    //var results = browser.tabs.executeScript(tab.id,{code:  'document.getElementsByTagName("link").innerText'})
+    var results = document.getElementsByTagName("link");
+    var activeTabUrl = document.getElementById('header-title');
+    var text = document.createTextNode("Ameaças at: " + tab.title);
+    var cookieList = document.getElementById('amecas-list');
+    activeTabUrl.appendChild(text);
+    let quantity = document.createElement("p");
+    let quantityText = document.createTextNode("links : "+results.length);
+    let quanlityText = document.createTextNode("link : "+results[0].href);
+    quantity.appendChild(quantityText);
+    quantity.appendChild(quanlityText);
+    cookieList.appendChild(quantity);
+
+
+  //   if (cookies.length > 0) {
+  //     for (let cookie of cookies) {
+  //       let li = document.createElement("li");
+  //       let content = document.createTextNode(cookie.name + ": "+ cookie.value);
+  //       li.appendChild(content);
+  //       cookieList.appendChild(li);
+  //     }
+
+  //     let quantity = document.createElement("p");
+  //     let quantityText = document.createTextNode("Total cookies: " + cookies.length);
+  //     quantity.appendChild(quantityText);
+  //     cookieList.appendChild(quantity);
+
+  //   } else {
+  //     let p = document.createElement("p");
+  //     let content = document.createTextNode("No cookies in this tab.");
+  //     let parent = cookieList.parentNode;
+
+  //     p.appendChild(content);
+  //     parent.appendChild(p);
+  //   }
+   //});
+}
+
+function getActiveTab() {
+  return browser.tabs.query({currentWindow: true, active: true});
+}
+
+getActiveTab().then(showCookiesForTab);
